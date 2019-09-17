8adb6c2358e8da9114d39cc9590fe2fb7d0d7005
Pulkit Midha <43686606+midopooler@users.noreply.github.com>
2019-09-17 12:56:18 +0530

    Add files via upload

---
+++ src/pattern16.java
@@ -0,0 +1,48 @@
+package patterns;
+
+import java.util.Scanner;
+
+/*
+*author Pulkit Midha
+*email midhapulkit28@gmail.com
+*date 20-Jun-2019
+*/
+public class pattern16 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+
+		int space = n - 1;
+		int star = n;
+		int row = 1;
+
+		while (row <= 2 * n - 1) {
+		
+			}
+			int csp = 1;
+			while (csp <= space) {
+				System.out.print(" ");
+				csp++;
+			}
+			int cst = 1;
+			while (cst <= star) {
+				System.out.print("*");
+				cst++;
+			}
+
+			System.out.println();
+
+			if (row <= n-1) {
+				star--;
+				space--;
+			} else {
+				star++;
+				space++;
+			}
+			row++;
+		}
+
+	}
+
+}
