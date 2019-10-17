8786ac38c9cd8ff89cb97b2b96fdd76b826ef888
Pulkit Midha <midhapulkit28@gmail.com>
2019-10-18 00:07:22 +0530

    Add files via upload

---
+++ src/Pattern29.java
@@ -0,0 +1,46 @@
+package patterns;
+
+import java.util.Scanner;
+
+/*
+*author Pulkit Midha
+*email midhapulkit28@gmail.com
+*date 21-Jun-2019
+*/
+public class Pattern29 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+
+		int space = n - 1;
+		int star = 1;
+		int var = 1;
+		int row = 1;
+		while (row <= n) {
+
+			int csp = 1;
+			while (csp <= space) {
+				System.out.print(" ");
+				csp = csp + 1;
+			}
+
+			int cst = 1;
+			while (cst <= star) {
+				if (cst == 1 || cst == 2 * row - 1) {
+					System.out.print(var);
+				} else {
+					System.out.print("0");
+				}
+				cst = cst + 1;
+			}
+			System.out.println();
+			star = star + 2;
+			space = space - 1;
+			var++;
+			row = row + 1;
+		}
+
+	}
+
+}
